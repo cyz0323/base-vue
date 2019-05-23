94f56c8419b4b3edacaa9cd1e2e3aa6f2cbc5346
work_yang <email_yangzeng@126.com>
2019-05-23 19:35:34 +0800

    自定义组件添加-定义表格

--- src/components/page/common/components/y-table.vue
+++ src/components/page/common/components/y-table.vue
@@ -1,43 +1,53 @@
 <template>
    <div>
        <el-table border :data="tableData" multiple style="width: 100%;" height="100%" >
-           <el-table-column fixed prop="index" label="序号" width="60" style="text-align: center;"></el-table-column>
-           <el-table-column prop="date" label="日期" width="150"></el-table-column>
-           <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-           <el-table-column prop="province" label="省份" width="120"></el-table-column>
-           <el-table-column prop="city" label="市区" width="120"></el-table-column>
-           <el-table-column prop="address" label="地址" width="300"></el-table-column>
-           <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+           <el-table-column :sortable="isSort" fixed prop="index" label="序号"></el-table-column>
+           <el-table-column :sortable="isSort" prop="date" label="日期"></el-table-column>
+           <el-table-column :sortable="isSort" prop="name" label="姓名"></el-table-column>
+           <el-table-column :sortable="isSort" prop="province" label="省份"></el-table-column>
+           <el-table-column :sortable="isSort" prop="city" label="市区"></el-table-column>
+           <el-table-column :sortable="isSort" prop="address" label="地址"></el-table-column>
+           <el-table-column :sortable="isSort" prop="zip" label="邮编"></el-table-column>
        </el-table>
        <div class="customposition">
-           <el-pagination background layout="prev, pager, next" :total="pageCount" :page-size="pageSize" :current-page="pageNo"></el-pagination>
+           <el-pagination background layout="prev, pager, next" @current-change="currentChange" @prev-click="prevClick" @next-click="nextClick"  :total="pageCount" :page-size="pageSize" :current-page="pageNo"></el-pagination>
+           <p class="pageSizeBar">总数：<span class="pageSizeText">{{pageCount}}</span>条</p>
        </div>
    </div>
 </template>
 
 <script>
     export default {
-        props: ["data","name"],
+        props: ["data","name","callback"],
         data(){
             return {
-                name: this.name,
-                param: this.data,
-                pageCount: 0,
-                pageSize: 0,
-                pageNo: 0,
-                tableData: []
+                key: this.name,     //唯一命名暂时不适用
+                param: this.data,   //参数列表
+                pageCount: 0,       //总数
+                pageSize: 0,        //每页数量
+                pageNo: 0,          //当前页数
+                tableData: [],      //表格数据
+                isSort: false,
+                currentChange: null,    //分页参数发生改变时触发
+                prevClick: null,        //分页点击上一页触发
+                nextClick: null         //分页点击下一页触发
             }
         },
         created(){
             this.tableData = this.param.data;
             this.pageCount = this.param.pageCount;
-            this.pageSize = this.param.pageSize;
+            this.pageSize = !!this.param.pageSize ? this.param.pageSize : 10;
             this.pageNo = this.param.pageNo;
-            this.tableData = this.param.data;
+            this.isSort = this.param.isSort;
+            this.currentChange = this.param.callback.currentChange;
+            this.prevClick = this.param.callback.prevClick;
+            this.nextClick = this.param.callback.nextClick;
         }
     }
 </script>
 
 <style scoped>
-    .customposition{text-align: right;margin: 20px 0;}
+    .customposition{text-align: right;margin: 20px 0;position: relative;}
+    .pageSizeBar{width: 200px;position: absolute; top: 5px;left: 10px;text-align: left;font-weight: bold;}
+    .pageSizeText{font-weight: normal;margin-right: 10px;}
 </style>
--- src/components/page/common/custom_table.vue
+++ src/components/page/common/custom_table.vue
@@ -10,22 +10,48 @@
                     <div class="demoModel">
                         <span class="head-title">描述：</span>
                         <p class="title">
-                            ------
+                            针对简单表的使用描述:<br/>
+                            1、利用import 对下拉组件进行引入<br/>
+                            2、在script 中对引入的组件进行定义<br/>
+                            3、在需要使用下拉的地方引入定义的标签<br/>
+                            参数说明：<br/>
+                            1、data：表格总数居列表详见js参数列表<br/>
+                            注：如果页面内需显示多个表格需import多次，同表单内下拉组件的定义一样
                         </p>
                         <span class="head-title">样例：</span>
                         <div class="ele" style="padding-left: 30px;">
-                            <y-table name="table1" :data="tableData"></y-table>
+                            <y-table :data="tableData"></y-table>
                         </div>
                         <span class="head-title">代码：</span>
                         <div class="code">
                             <span class="code-msg">html</span>
                             <div class="code-main">
-                                -----
+                                &lt;y-table :data="tableData"&gt;&lt;/y-table&gt;
                             </div>
                             <span class="code-msg">script</span>
                             <div class="code-main">
 <pre>
-
+import yTable from './components/y-table';
+export default{
+    components: {yTable},
+    data(){
+        return {
+            title: "自定义数据表格",
+            activeName: 'first-ele',
+            tableData: {
+                pageCount: 41,  //记录总数
+                pageNo: 1,  //当前页数
+                isSort: true,   //是否支持排序
+                data: [],   //表格数据
+                callback: {
+                    currentChange: function(e){console.log(e);},                 //分页参数发生改变触发
+                    prevClick: function(e){console.log("外上一页："+e);},        //分页上一页点击触发
+                    nextClick: function(e){console.log("外下一页："+e); }        //分页下一页点击触发
+                }
+            },
+        }
+    }
+}
 </pre>
                             </div>
                         </div>
@@ -96,72 +122,28 @@
             return {
                 title: "自定义数据表格",
                 activeName: 'first-ele',
-                tableCounts: 40,
-                pageNo: 1,
                 tableData: {
-                    pageCount: 40,
-                    pageSize: 10,
-                    pageNo: 1,
+                    pageCount: 41,  //记录总数
+                    pageNo: 1,  //当前页数
+                    isSort: true,   //是否支持排序
                     data: [
-                        {
-                            index: '1',
-                            date: '2016-05-03',
-                            name: '王小虎',
-                            province: '上海',
-                            city: '普陀区',
-                            address: '上海市普陀区金沙江路 1518 弄',
-                            zip: 200333
-                        },{
-                            index: '2',
-                            date: '2016-05-02',
-                            name: '王小虎',
-                            province: '上海',
-                            city: '普陀区',
-                            address: '上海市普陀区金沙江路 1518 弄',
-                            zip: 200333
-                        }, {
-                            index: '3',
-                            date: '2016-05-04',
-                            name: '王小虎',
-                            province: '上海',
-                            city: '普陀区',
-                            address: '上海市普陀区金沙江路 1518 弄',
-                            zip: 200333
-                        }, {
-                            index: '4',
-                            date: '2016-05-01',
-                            name: '王小虎',
-                            province: '上海',
-                            city: '普陀区',
-                            address: '上海市普陀区金沙江路 1518 弄',
-                            zip: 200333
-                        }, {
-                            index: '5',
-                            date: '2016-05-08',
-                            name: '王小虎',
-                            province: '上海',
-                            city: '普陀区',
-                            address: '上海市普陀区金沙江路 1518 弄',
-                            zip: 200333
-                        }, {
-                            index: '6',
-                            date: '2016-05-06',
-                            name: '王小虎',
-                            province: '上海',
-                            city: '普陀区',
-                            address: '上海市普陀区金沙江路 1518 弄',
-                            zip: 200333
-                        }, {
-                            index: '7',
-                            date: '2016-05-07',
-                            name: '王小虎',
-                            province: '上海',
-                            city: '普陀区',
-                            address: '上海市普陀区金沙江路 1518 弄',
-                            zip: 200333
-                        }
-                    ]
-                }
+                        {index: '1',date: '2016-05-03',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄',zip: 200333},
+                        {index: '2',date: '2016-05-02',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄',zip: 200333},
+                        {index: '3',date: '2016-05-04',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄', zip: 200333},
+                        {index: '4',date: '2016-05-01',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄',zip: 200333 },
+                        {index: '5',date: '2016-05-08',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄',zip: 200333 },
+                        {index: '6',date: '2016-05-06',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄', zip: 200333},
+                        {index: '7',date: '2016-05-07',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄',zip: 200333},
+                        {index: '8',date: '2016-05-07',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄',zip: 200333},
+                        {index: '9',date: '2016-05-07',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄',zip: 200333},
+                        {index: '10',date: '2016-05-07',name: '王小虎',province: '上海',city: '普陀区',address: '上海市普陀区金沙江路 1518 弄',zip: 200333}
+                    ],
+                    callback: {
+                        currentChange: function(e){console.log(e);},
+                        prevClick: function(e){console.log("外上一页："+e);},
+                        nextClick: function(e){console.log("外下一页："+e); }
+                    }
+                },
             }
         },
         created(){
