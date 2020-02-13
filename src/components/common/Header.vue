5c23149b868bfc53d57c77b72531219d28ad6e02
pc_yang <email_yangzeng@126.com>
2020-02-13 16:53:58 +0800

    首页版式修改

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -110,7 +110,7 @@
         position: relative;
         box-sizing: border-box;
         width: 100%;
-        height: 70px;
+        height: 60px;
         font-size: 22px;
         color: #fff;
     }
@@ -118,12 +118,12 @@
         float: left;
         padding: 0 21px;
         cursor: pointer;
-        line-height: 70px;
+        line-height: 60px;
     }
     .header .logo{
         float: left;
         width:250px;
-        line-height: 70px;
+        line-height: 60px;
     }
     .header-right{
         float: right;
@@ -131,7 +131,7 @@
     }
     .header-user-con{
         display: flex;
-        height: 70px;
+        height: 60px;
         align-items: center;
     }
     .btn-fullscreen{
--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -55,8 +55,8 @@
 </script>
 
 <style scoped>
-    .sidebar{display: block;position: absolute;left: 0;top: 70px;bottom:0;overflow-y: scroll;}
+    .sidebar{display: block;position: absolute;left: 0;top: 60px;bottom:0;overflow-y: scroll;}
     .sidebar::-webkit-scrollbar{ width: 0; }
-    .sidebar-el-menu:not(.el-menu--collapse){width: 250px;}
+    .sidebar-el-menu:not(.el-menu--collapse){width: 200px;}
     .sidebar > ul {height:100%;}
 </style>
